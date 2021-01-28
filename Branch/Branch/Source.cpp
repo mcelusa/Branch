13492ef94ae3682a9a3c23ddbc8118c076e49478
made-in-nagasaki <k020g1095@g.neec.ac.jp>
2021-01-28 11:51:39 +0900

    Merge branch 'master' of https://github.com/mcelusa/Branch
    # Conflicts:
    #	Branch/Branch/Source.cpp

--- Branch/Branch/Source.cpp
+++ Branch/Branch/Source.cpp
@@ -5,9 +5,14 @@ using namespace std;
 int main () {
 
 	cout << "ƒ}ƒ‹‘‚¢‚½" << endl;
+<<<<<<< HEAD
 	for (int i = 0; i < 256; i++)
 	{
 		printf("–°‚¢")
 	}
 	return 0;
+=======
+	
+	cout << "Žæ‚èÁ‚µ‚½" << endl;
+>>>>>>> fa0c11a85b6f819be915fbea0cbc8d39368f71a5
 }
