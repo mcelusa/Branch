ffa571c17b79091f63de747ba315747cc8b948a4
made-in-nagasaki <k020g1095@g.neec.ac.jp>
2021-01-28 11:33:46 +0900

    追加しました

--- Branch/Branch/Source.cpp
+++ Branch/Branch/Source.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+#include<stdio.h>
 using namespace std;
 
 int main () {
 
 	cout << "ƒ}ƒ‹‘‚¢‚½" << endl;
-	
+	for (int i = 0; i < 256; i++)
+	{
+		printf("–°‚¢")
+	}
+	return 0;
 }
